1590b8dcaf83578c669f088ba326a818ccda0819
Julian Chultarsky <jchultarsky@physna.com>
2025-10-13 20:20:06 -0400

    Improve banner with better gradient colors and spacing

--- src/banner.rs
+++ src/banner.rs
@@ -5,16 +5,18 @@ pub fn print_banner() {
     println!();
     cprintln!("<r>PCLI2</r> - Physna Command Line Interface v2");
     println!();
+    println!();
     
-    // Print ASCII art spelling out "PCLI2" with reversed gradient orange colors (lighter on top)
+    // Print ASCII art spelling out "PCLI2" with smooth gradient from light to dark orange (top to bottom)
     cprintln!("<#FFA500> ███████████    █████████  █████       █████  ████████ </#FFA500>");
-    cprintln!("<#FF9900>░░███░░░░░███  ███░░░░░███░░███       ░░███  ███░░░░███</#FF9900>");
+    cprintln!("<#FF9500>░░███░░░░░███  ███░░░░░███░░███       ░░███  ███░░░░███</#FF9500>");
     cprintln!("<#FF8C00> ░███    ░███ ███     ░░░  ░███        ░███ ░░░    ░███</#FF8C00>");
     cprintln!("<#FF8000> ░██████████ ░███          ░███        ░███    ███████ </#FF8000>");
     cprintln!("<#FF7200> ░███░░░░░░  ░███          ░███        ░███   ███░░░░  </#FF7200>");
     cprintln!("<#FF6300> ░███        ░░███     ███ ░███      █ ░███  ███      █</#FF6300>");
     cprintln!("<#FF5500> █████        ░░█████████  ███████████ █████░██████████</#FF5500>");
     cprintln!("<#FF4500>░░░░░          ░░░░░░░░░  ░░░░░░░░░░░ ░░░░░ ░░░░░░░░░░ </#FF4500>");
+    println!();
     println!();
 }
 
